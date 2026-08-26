c8070a55b034931c9f7c997831c481e2b609e630
TracyZhang-27 <zhangchunyixox@gmail.com>
2026-08-26 15:49:58 +1000

    Update SelectionUIController.cs

--- Prototype1/Assets/Project/Scripts/UI/SelectionUIController.cs
+++ Prototype1/Assets/Project/Scripts/UI/SelectionUIController.cs
@@ -3,22 +3,52 @@ using TMPro;
 
 public class SelectionUIController : MonoBehaviour
 {
+    [Header("Text")]
     [SerializeField]
     private TMP_Text selectedInsectName;
 
     [SerializeField]
+    private TMP_Text selectionHint;
+
+
+    [Header("Button")]
+    [SerializeField]
     private GameObject confirmButton;
 
-    public void ShowSelection(string insectName)
-    {
-        selectedInsectName.text = insectName;
 
-        confirmButton.SetActive(true);
+    public void ShowSelection(
+        string insectName,
+        bool canExplore
+    )
+    {
+        selectedInsectName.text =
+            insectName.ToUpper();
+
+
+        if (canExplore)
+        {
+            selectionHint.text =
+                "Ready to explore";
+
+            confirmButton.SetActive(true);
+        }
+        else
+        {
+            selectionHint.text =
+                "COMING SOON";
+
+            confirmButton.SetActive(false);
+        }
     }
+
 
     public void ClearSelection()
     {
-        selectedInsectName.text = "SELECT AN INSECT";
+        selectedInsectName.text =
+            "SELECT AN INSECT";
+
+        selectionHint.text =
+            "Choose one to explore";
 
         confirmButton.SetActive(false);
     }
