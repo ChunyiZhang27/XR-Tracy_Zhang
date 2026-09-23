bb6a90ccfe8558177e52454ff89ff9e68c5ec6be
TracyZhang-27 <zhangchunyixox@gmail.com>
2026-09-23 21:17:56 +1000

    Update SFXManager.cs

--- Prototype1/Assets/Project/Scripts/Audio/SFXManager.cs
+++ Prototype1/Assets/Project/Scripts/Audio/SFXManager.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class SFXManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Audio Source")]
+    [SerializeField]
+    private AudioSource sfxSource;
 
-    // Update is called once per frame
-    void Update()
+
+    [Header("SFX Clips")]
+    [SerializeField]
+    private AudioClip wingFlapSFX;
+
+
+    // =========================
+    // WING FLAP
+    // =========================
+
+    public void PlayWingFlap()
     {
-        
+        if (sfxSource == null)
+        {
+            return;
+        }
+
+        if (wingFlapSFX == null)
+        {
+            return;
+        }
+
+        // PlayOneShot 不会修改 AudioSource 当前 clip
+        // 很适合这种短暂动作音效
+        sfxSource.PlayOneShot(
+            wingFlapSFX
+        );
     }
 }
